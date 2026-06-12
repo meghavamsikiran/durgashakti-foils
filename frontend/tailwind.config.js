11cba95332bdb4d94745d36cf33eccdcb0ead6ae
meghavamsikiran <meghavamsikiran@gmail.com>
2026-06-12 14:07:44 +0530

    Configure global headings to use premium Playfair Display font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
         sans: ['Inter', 'sans-serif'],
         manrope: ['Manrope', 'sans-serif'],
         mono: ['JetBrains Mono', 'monospace'],
+        serif: ['Playfair Display', 'serif'],
       },
   		colors: {
   			background: 'hsl(var(--background))',
