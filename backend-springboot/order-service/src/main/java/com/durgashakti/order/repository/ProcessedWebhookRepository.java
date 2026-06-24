f54eeb45beee9bc3ae9c29582911055a01023cc4
Your Name <you@example.com>
2026-06-24 17:22:52 +0530

    fix: correct ProcessedWebhookRepository to use eventId field and String ID type

--- backend-springboot/order-service/src/main/java/com/durgashakti/order/repository/ProcessedWebhookRepository.java
+++ backend-springboot/order-service/src/main/java/com/durgashakti/order/repository/ProcessedWebhookRepository.java
@@ -4,9 +4,7 @@ import com.durgashakti.common.entity.ProcessedWebhook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface ProcessedWebhookRepository extends JpaRepository<ProcessedWebhook, UUID> {
-    boolean existsByWebhookId(String webhookId);
+public interface ProcessedWebhookRepository extends JpaRepository<ProcessedWebhook, String> {
+    boolean existsByEventId(String eventId);
 }
